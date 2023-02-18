6fbc00cb261c9c28f18b869bbc92f5a7b36bf713
Fernando Antonio <fernandojr.semrhfj@hotmail.com>
2023-02-18 15:15:46 +0000

    fix: fix mapping to work on windows

--- RetroBitMegaDriveVisualizer.py
+++ RetroBitMegaDriveVisualizer.py
@@ -16,13 +16,13 @@ keep_playing = True
 pressed = False
 
 #Setting icon and title
-icon = pygame.image.load(os.getcwd() + "/images/icon.png")
+icon = pygame.image.load(os.getcwd() + "\\images\\icon.png")
 pygame.display.set_icon(icon)
 pygame.display.set_caption("Retro-Bit Mega Drive Controller")
 
 #Setting up main window
 screen = pygame.display.set_mode((800, 600))
-bg = pygame.image.load(os.getcwd() + "/images/background.png").convert_alpha()
+bg = pygame.image.load(os.getcwd() + "\\images\\background.png").convert_alpha()
 main_display = pygame.display.set_mode(bg.get_size())
 main_display.blit(bg, (0,0))
 pygame.display.flip()
@@ -35,7 +35,7 @@ for i in range(0, pygame.joystick.get_count()):
 def draw_pressed_buttons():
     main_display.blit(bg, (0,0))
     for held_button in held_buttons:
-        main_display.blit(pygame.image.load(os.getcwd() + "/images/"+held_button+".png").convert_alpha(), (0,0))
+        main_display.blit(pygame.image.load(os.getcwd() + "\\images\\"+held_button+".png").convert_alpha(), (0,0))
     pygame.display.flip()
     
 def button_down(name):
@@ -50,6 +50,7 @@ def button_up(name):
 
 while keep_playing:
     for event in pygame.event.get():
+        print(event)
         if event.type == pygame.QUIT:
             pygame.display.quit()
             pygame.quit()
@@ -57,61 +58,61 @@ while keep_playing:
             keep_playing = False
                 
         elif event.type == pygame.JOYAXISMOTION:
-            if event.axis == 5 and event.value > 0:
-                button_down("c")
-            elif event.axis == 5 and event.value < 0:
-                button_up("c")
-            elif event.axis == 4 and event.value > 0:
-                button_down("z")
-            elif event.axis == 4 and event.value < 0:
-                button_up("z")
-            elif event.axis == 0 and event.value < 0.5 and event.value > -0.5:
+            if event.axis == 0 and event.value < 0.5 and event.value > -0.5:
                 button_up("left")
                 button_up("right")
             elif event.axis == 0 and event.value < 0:
                 button_down("left")
             elif event.axis == 0 and event.value > 0:
                 button_down("right")
-            elif event.axis == 1 and event.value < 0.5 and event.value > -0.5:
+            elif event.axis == 4 and event.value < 0.5 and event.value > -0.5:
                 button_up("up")
                 button_up("down")
-            elif event.axis == 1 and event.value < 0:
+            elif event.axis == 4 and event.value < 0:
                 button_down("up")
-            elif event.axis == 1 and event.value > 0:
+            elif event.axis == 4 and event.value > 0:
                 button_down("down")
         
         elif event.type == pygame.JOYBUTTONDOWN:
             if event.button == 0:
-                button_down("a")
+                button_down("x")
             elif event.button == 1:
-                button_down("b")
+                button_down("a")
             elif event.button == 2:
-                button_down("x")    
+                button_down("b")    
             elif event.button == 3:
                 button_down("y")
             elif event.button == 4:
-                button_down("mode")
+                button_down("c")
+            elif event.button == 5:
+                button_down("z")
             elif event.button == 6:
-                button_down("start")    
-            elif event.button == 9:
                 button_down("l")
-            elif event.button == 10:
+            elif event.button == 7:
                 button_down("r")
+            elif event.button == 8:
+                button_down("mode")
+            elif event.button == 9:
+                button_down("start")
                 
         elif event.type == pygame.JOYBUTTONUP:
             if event.button == 0:
-                button_up("a")
-            elif event.button == 1:
-                button_up("b")  
-            elif event.button == 2:
                 button_up("x")
+            elif event.button == 1:
+                button_up("a")
+            elif event.button == 2:
+                button_up("b")    
             elif event.button == 3:
                 button_up("y")
             elif event.button == 4:
-                button_up("mode")
+                button_up("c")
+            elif event.button == 5:
+                button_up("z")
             elif event.button == 6:
-                button_up("start")    
-            elif event.button == 9:
                 button_up("l")
-            elif event.button == 10:
+            elif event.button == 7:
                 button_up("r")
+            elif event.button == 8:
+                button_up("mode")
+            elif event.button == 9:
+                button_up("start")
