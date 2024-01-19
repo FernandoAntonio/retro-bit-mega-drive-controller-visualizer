ceaeb82138122e95ff9be8cf847ae813627e610f
Fernando Antonio <fernandojr.semrhfj@hotmail.com>
2024-01-19 01:57:45 -0300

    add macos support and themes in compilation time

--- RetroBitMegaDriveVisualizer.py
+++ RetroBitMegaDriveVisualizer.py
@@ -1,15 +1,13 @@
 import os
 
-#Needs to be set before importing pygame, or can also be set from shell
-#while calling the script
+# Needs to be set before importing pygame, or can also be set from shell
+# while calling the script
 os.environ["SDL_JOYSTICK_ALLOW_BACKGROUND_EVENTS"] = "1"
 
+import platform
 import pygame
-import win32api
-import win32con
-import win32gui
 
-#Starting
+# Starting
 pygame.init()
 joysticks = []
 held_buttons = []
@@ -17,108 +15,197 @@ clock = pygame.time.Clock()
 clock.tick(60)
 keep_playing = True
 pressed = False
+theme = "overlay_grey" # "overlay_subtle" # "overlay_white"
 
-#Setting icon and title
+# Setting icon and title
 icon = pygame.image.load(os.path.join(os.getcwd(), "images", "icon.png"))
 pygame.display.set_icon(icon)
 pygame.display.set_caption("Retro-Bit Mega Drive Controller")
 
+# Setting up main window
+if platform.system() == "Windows":
+    import win32api
+    import win32con
+    import win32gui
 
-#Setting up main window
-hwnd = pygame.display.get_wm_info()["window"]
-win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE,win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
-win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*(255, 0, 128)), 0, win32con.LWA_COLORKEY)
-bg = pygame.image.load(os.path.join(os.getcwd(), "images", "background.png")).convert_alpha()
-main_display = pygame.display.set_mode(bg.get_size(), pygame.NOFRAME)
+    hwnd = pygame.display.get_wm_info()["window"]
+    win32gui.SetWindowLong(
+        hwnd,
+        win32con.GWL_EXSTYLE,
+        win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED,
+    )
+    win32gui.SetLayeredWindowAttributes(
+        hwnd, win32api.RGB(*(255, 0, 128)), 0, win32con.LWA_COLORKEY
+    )
+
+bg = pygame.image.load(os.path.join(os.getcwd(), "images", "background.png"))
+main_display = pygame.display.set_mode(bg.get_size())
 main_display.fill((255, 0, 128))
-main_display.blit(bg, (0,0))
+main_display.blit(bg, (0, 0))
 pygame.display.flip()
-        
-#For all the connected joysticks
+
+# For all the connected joysticks
 for i in range(0, pygame.joystick.get_count()):
     joysticks.append(pygame.joystick.Joystick(i))
     joysticks[-1].init()
- 
+
+
 def draw_pressed_buttons():
-    main_display.blit(bg, (0,0))
+    main_display.blit(bg, (0, 0))
     for held_button in held_buttons:
-        main_display.blit(pygame.image.load(os.path.join(os.getcwd(), "images", held_button + ".png")).convert_alpha(), (0,0))
+        main_display.blit(
+            pygame.image.load(
+                os.path.join(os.getcwd(), "images", theme, held_button + ".png")
+            ).convert_alpha(),
+            (0, 0),
+        )
     pygame.display.flip()
-    
+
+
 def button_down(name):
     held_buttons.append(name)
     draw_pressed_buttons()
-    
+
+
 def button_up(name):
     for button in held_buttons:
         if button == name:
             held_buttons.remove(name)
     draw_pressed_buttons()
 
+
 while keep_playing:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.display.quit()
             pygame.quit()
-            done = True
             keep_playing = False
-                
-        elif event.type == pygame.JOYAXISMOTION:
-            if event.axis == 0 and event.value < 0.5 and event.value > -0.5:
-                button_up("left")
-                button_up("right")
-            elif event.axis == 0 and event.value < 0:
-                button_down("left")
-            elif event.axis == 0 and event.value > 0:
-                button_down("right")
-            elif event.axis == 4 and event.value < 0.5 and event.value > -0.5:
-                button_up("up")
-                button_up("down")
-            elif event.axis == 4 and event.value < 0:
-                button_down("up")
-            elif event.axis == 4 and event.value > 0:
-                button_down("down")
-        
-        elif event.type == pygame.JOYBUTTONDOWN:
-            if event.button == 0:
-                button_down("x")
-            elif event.button == 1:
-                button_down("a")
-            elif event.button == 2:
-                button_down("b")    
-            elif event.button == 3:
-                button_down("y")
-            elif event.button == 4:
-                button_down("c")
-            elif event.button == 5:
-                button_down("z")
-            elif event.button == 6:
-                button_down("l")
-            elif event.button == 7:
-                button_down("r")
-            elif event.button == 8:
-                button_down("mode")
-            elif event.button == 9:
-                button_down("start")
-                
-        elif event.type == pygame.JOYBUTTONUP:
-            if event.button == 0:
-                button_up("x")
-            elif event.button == 1:
-                button_up("a")
-            elif event.button == 2:
-                button_up("b")    
-            elif event.button == 3:
-                button_up("y")
-            elif event.button == 4:
-                button_up("c")
-            elif event.button == 5:
-                button_up("z")
-            elif event.button == 6:
-                button_up("l")
-            elif event.button == 7:
-                button_up("r")
-            elif event.button == 8:
-                button_up("mode")
-            elif event.button == 9:
-                button_up("start")
+
+        if platform.system() == "Darwin":
+            if event.type == pygame.KEYUP and event.key == 27:
+                pygame.display.quit()
+                pygame.quit()
+                keep_playing = False
+
+            if event.type == pygame.JOYAXISMOTION:
+                if event.axis == 0 and event.value < 0:
+                    button_down("left")
+                if event.axis == 0 and event.value > 0:
+                    button_up("left")
+                if event.axis == 0 and event.value > 0.5:
+                    button_down("right")
+                if event.axis == 0 and event.value < 0.5:
+                    button_up("right")
+                if event.axis == 1 and event.value < 0:
+                    button_down("up")
+                if event.axis == 1 and event.value > 0:
+                    button_up("up")
+                if event.axis == 1 and event.value > 0.5:
+                    button_down("down")
+                if event.axis == 1 and event.value < 0.5:
+                    button_up("down")
+                if event.axis == 5 and event.value > 0:
+                    button_down("c")
+                if event.axis == 5 and event.value < 0:
+                    button_up("c")
+                if event.axis == 4 and event.value > 0:
+                    button_down("z")
+                if event.axis == 4 and event.value < 0:
+                    button_up("z")
+
+            if event.type == pygame.JOYBUTTONDOWN:
+                if event.button == 0:
+                    button_down("a")
+                if event.button == 1:
+                    button_down("b")
+                if event.button == 2:
+                    button_down("x")
+                if event.button == 3:
+                    button_down("y")
+                if event.button == 4:
+                    button_down("mode")
+                if event.button == 6:
+                    button_down("start")
+                if event.button == 9:
+                    button_down("l")
+                if event.button == 10:
+                    button_down("r")
+
+            if event.type == pygame.JOYBUTTONUP:
+                if event.button == 0:
+                    button_up("a")
+                if event.button == 1:
+                    button_up("b")
+                if event.button == 2:
+                    button_up("x")
+                if event.button == 3:
+                    button_up("y")
+                if event.button == 4:
+                    button_up("mode")
+                if event.button == 6:
+                    button_up("start")
+                if event.button == 9:
+                    button_up("l")
+                if event.button == 10:
+                    button_up("r")
+
+        if platform.system() == "Windows":
+            if event.type == pygame.JOYAXISMOTION:
+                if event.axis == 0 and event.value < 0.5 and event.value > -0.5:
+                    button_up("left")
+                    button_up("right")
+                if event.axis == 0 and event.value < 0:
+                    button_down("left")
+                if event.axis == 0 and event.value > 0:
+                    button_down("right")
+                if event.axis == 4 and event.value < 0.5 and event.value > -0.5:
+                    button_up("up")
+                    button_up("down")
+                if event.axis == 4 and event.value < 0:
+                    button_down("up")
+                if event.axis == 4 and event.value > 0:
+                    button_down("down")
+
+            if event.type == pygame.JOYBUTTONDOWN:
+                if event.button == 0:
+                    button_down("x")
+                if event.button == 1:
+                    button_down("a")
+                if event.button == 2:
+                    button_down("b")
+                if event.button == 3:
+                    button_down("y")
+                if event.button == 4:
+                    button_down("c")
+                if event.button == 5:
+                    button_down("z")
+                if event.button == 6:
+                    button_down("l")
+                if event.button == 7:
+                    button_down("r")
+                if event.button == 8:
+                    button_down("mode")
+                if event.button == 9:
+                    button_down("start")
+
+            if event.type == pygame.JOYBUTTONUP:
+                if event.button == 0:
+                    button_up("x")
+                if event.button == 1:
+                    button_up("a")
+                if event.button == 2:
+                    button_up("b")
+                if event.button == 3:
+                    button_up("y")
+                if event.button == 4:
+                    button_up("c")
+                if event.button == 5:
+                    button_up("z")
+                if event.button == 6:
+                    button_up("l")
+                if event.button == 7:
+                    button_up("r")
+                if event.button == 8:
+                    button_up("mode")
+                if event.button == 9:
+                    button_up("start")
